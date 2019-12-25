bfbe6c0a57c58fd816ce6ae7ed8683de67268137
rohitm94 <rohit11094@gmail.com>
2019-12-24 21:59:46 -0500

    Updates

---
+++ precache-manifest.dbe364d7653981e6c850a68d93ce7822.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdd22713eb420e7c63b2a16b03722cc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "54baeb5cad0ac8bfad2c",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "68637e67f45e51162f5e",
+    "url": "/portfolio/static/css/main.40fc4be4.chunk.css"
+  },
+  {
+    "revision": "54baeb5cad0ac8bfad2c",
+    "url": "/portfolio/static/js/2.38889772.chunk.js"
+  },
+  {
+    "revision": "777af54479ed5c3b0967650bf3d8a7c5",
+    "url": "/portfolio/static/js/2.38889772.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68637e67f45e51162f5e",
+    "url": "/portfolio/static/js/main.e83e22d3.chunk.js"
+  },
+  {
+    "revision": "4bcc3983bbdf3794ec1c",
+    "url": "/portfolio/static/js/runtime-main.6fcaddba.js"
+  }
+]);
